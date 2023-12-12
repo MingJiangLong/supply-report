e36df0c9aae91d436aeca7bb1d504f9f57c73a68
mingjianglong <l502858283@gmail.com>
2023-12-12 11:07:05 +0800

    feat: 增加点击图片返回功能

--- src/pages/ImagePreview.vue
+++ src/pages/ImagePreview.vue
@@ -1,11 +1,16 @@
 <template>
-  <img :src="shareData.displayImage" />
+  <img :src="shareData.displayImage" @click="onGoBack" />
 </template>
 
 <script setup lang="ts">
 import { useShareData } from "@/store"
-import {} from "vue"
+import { } from "vue"
+import { useRouter } from "vue-router";
 const shareData = useShareData()
+const router = useRouter()
+function onGoBack() {
+  router.back()
+}
 </script>
 
 <style scoped lang="less">
