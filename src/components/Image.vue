f6345bbd84e2df21b44ff1b3a374e44410cb110f
mingjianglong <l502858283@gmail.com>
2023-12-22 13:50:46 +0800

    feat: 调整pre环境地址

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -108,7 +108,7 @@ async function onInput(e: any) {
           loading.value = false
           emit("afterUpload", result?.url)
         })
-        .catch((error:any) => {
+        .catch((_error:any) => {
           loading.value = false
           showToast({ message: "上传失败", type: "fail" })
         })
