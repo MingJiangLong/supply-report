6eab650962b4992224702b0c4c144b309641b598
mingjianglong <l502858283@gmail.com>
2024-03-20 14:39:58 +0800

    feat: 临时路径

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export const API_HOST = import.meta.env.VITE_APP_API_HOST
+export const APP_BASE_PATH = import.meta.env.VVITE_APP_BASE_PATH
 
 export const NORMAL_SUPPLY_STEPS = ["开门前拍照", "补货后上报", "补货后拍照"]
 export const COUNT_SUPPLY_STEPS = ["开门前拍照", "补货前盘点", "补货后上报", "补货后拍照"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,85 +1,86 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import vue from "@vitejs/plugin-vue"
 import legacy from "@vitejs/plugin-legacy"
 import path from "path"
 import viteCompression from "vite-plugin-compression"
 const isProd = process.argv[process.argv.length - 1] == "prod"
 const isPre = process.argv[process.argv.length - 1] == "pre"
-
 function getSuffix() {
   if (isProd) return ""
   if (isPre) return "-pre"
   return "-dev"
 }
-export default defineConfig({
-  base: `/supply-report${getSuffix()}`,
-  plugins: [
-    vue(),
-    legacy({
-      targets: ["defaults", "ie >= 11", "chrome 52"], //需要兼容的目标列表，可以设置多个
-      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
-      renderLegacyChunks: true,
-      polyfills: [
-        "es.symbol",
-        "es.array.filter",
-        "es.promise",
-        "es.promise.finally",
-        "es/map",
-        "es/set",
-        "es.array.for-each",
-        "es.object.define-properties",
-        "es.object.define-property",
-        "es.object.get-own-property-descriptor",
-        "es.object.get-own-property-descriptors",
-        "es.object.keys",
-        "es.object.to-string",
-        "web.dom-collections.for-each",
-        "esnext.global-this",
-        "esnext.string.match-all",
-      ],
-    }),
-    viteCompression(),
-  ],
+export default ({ mode }) => {
+  return defineConfig({
+    base: loadEnv(mode, process.cwd()).VITE_APP_BASE_PATH,
+    plugins: [
+      vue(),
+      legacy({
+        targets: ["defaults", "ie >= 11", "chrome 52"], //需要兼容的目标列表，可以设置多个
+        additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+        renderLegacyChunks: true,
+        polyfills: [
+          "es.symbol",
+          "es.array.filter",
+          "es.promise",
+          "es.promise.finally",
+          "es/map",
+          "es/set",
+          "es.array.for-each",
+          "es.object.define-properties",
+          "es.object.define-property",
+          "es.object.get-own-property-descriptor",
+          "es.object.get-own-property-descriptors",
+          "es.object.keys",
+          "es.object.to-string",
+          "web.dom-collections.for-each",
+          "esnext.global-this",
+          "esnext.string.match-all",
+        ],
+      }),
+      viteCompression(),
+    ],
 
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  build: {
-    outDir: `../h5/supply-report${getSuffix()}`,
-    manifest: true,
-    emptyOutDir: true,
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: isProd ? true : false,
-        drop_debugger: isProd ? true : false,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
       },
     },
-    chunkSizeWarningLimit: 5000,
-    rollupOptions: {
-      output: {
-        entryFileNames: "assets/entry/[name][hash].js",
-        chunkFileNames: "assets/chunk/[name][hash].js",
-        assetFileNames: "assets/file/[name][hash].[ext]",
-        manualChunks(id) {
-          const NODE_MODULES = "node_modules"
-          if (id.includes(NODE_MODULES)) return "vendor"
+    build: {
+      outDir: `../h5${loadEnv(mode, process.cwd()).VITE_APP_BASE_PATH}`,
+      manifest: true,
+      emptyOutDir: true,
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          drop_console: isProd ? true : false,
+          drop_debugger: isProd ? true : false,
+        },
+      },
+      chunkSizeWarningLimit: 5000,
+      rollupOptions: {
+        output: {
+          entryFileNames: "assets/entry/[name][hash].js",
+          chunkFileNames: "assets/chunk/[name][hash].js",
+          assetFileNames: "assets/file/[name][hash].[ext]",
+          manualChunks(id) {
+            const NODE_MODULES = "node_modules"
+            if (id.includes(NODE_MODULES)) return "vendor"
+          },
         },
       },
     },
-  },
-  server: {
-    host: "0.0.0.0",
-    proxy: {
-      "/escort": {
-        target: "https://api.dev.uboxol.com",
-        changeOrigin: true,
-        // rewrite(path) {
-        //   return path.replace('/escort-pre', '')
-        // },
+    server: {
+      host: "0.0.0.0",
+      proxy: {
+        "/escort": {
+          target: "https://api.dev.uboxol.com",
+          changeOrigin: true,
+          // rewrite(path) {
+          //   return path.replace('/escort-pre', '')
+          // },
+        },
       },
     },
-  },
-})
+  })
+}
